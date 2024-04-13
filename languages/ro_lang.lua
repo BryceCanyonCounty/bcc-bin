5d7132bd69c77841cbf8ff1ceae374b5c4ff3b22
iseeyoucopy <2729972+iseeyoucopy@users.noreply.github.com>
2024-04-14 00:17:01 +0100

    Refactor code (#2)
    * Refactor code
    
    - added Romanian language support
    - changed Vorputils to BccUtils
    - added webhook title and avatar to config
    - added localization language keys for webhook
    -changed vector3 in config
    -updated VORP includes to new callback system
    
    * added label for item
    
    - added itemName and itemLabel to config

---
+++ languages/ro_lang.lua
@@ -0,0 +1,14 @@
+---------------------------------- ROMANIAN -------------------------------------------
+Locales["ro_lang"] = {
+    bin = "Cos de gunoi",
+    search = 'Cauta',
+    progressbar = 'Cauta in cos',
+    moneyfound = 'Ai gasit bani',
+    itemfound = 'Ai gasit',
+    weaponfound = 'Ai gasit o arma',
+    webhookName = 'Nume&Prenume: ',
+    webhookIdentifier = '\nIdentifier:',
+    webhookRewardM = '\nRecompensa bani: ',
+    webhookRewardI = '\nRecompensa obiect: ',
+    webhookRewardW = '\nRecompensa Arma: ',
+}
