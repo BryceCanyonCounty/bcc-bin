ccbc0349294607dfdece3e23ea6c6e5ce8ef2090
Elzetia <cy.buniet@gmail.com>
2024-09-22 00:16:34 +0200

    Create fr_lang.lua (#1)

---
+++ languages/fr_lang.lua
@@ -0,0 +1,9 @@
+---------------------------------- French translation by #slipknot4512 ----------------------------------
+Locales["fr_lang"] = {
+bin = "Poubelle",
+search = 'Rechercher',
+progressbar = 'Recherche dans la poubelle',
+moneyfound = 'Argent trouvé',
+itemfound = 'Objet trouvé',
+weaponfound = 'Arme trouvée'
+}
