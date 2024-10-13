d298bb50ccd5f1f795a6e8cb6e4cc81b173eda75
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-12 23:20:17 -0700

    Update fr_lang.lua

--- languages/fr_lang.lua
+++ languages/fr_lang.lua
@@ -1,9 +1,16 @@
 ---------------------------------- French translation by #slipknot4512 ----------------------------------
 Locales["fr_lang"] = {
-bin = "Poubelle",
-search = 'Rechercher',
-progressbar = 'Recherche dans la poubelle',
-moneyfound = 'Argent trouvé',
-itemfound = 'Objet trouvé',
-weaponfound = 'Arme trouvée'
+    bin = "Poubelle",
+    search = "Rechercher",
+    progressbar = "Recherche dans la poubelle",
+    moneyfound = "Argent trouvé: $",
+    itemfound = "Objet trouvé: ",
+    weaponfound = "Arme trouvée: ",
+    webhookName = "Firstname&Lastname: ",
+    webhookIdentifier = "\nIdentifier:",
+    webhookRewardM = "\nReward Money: ",
+    webhookRewardI = "\nReward Item: ",
+    webhookRewardW = "\nReward Weapon: ",
+    noSpace = "You do not have enough space in your inventory.",
+    useCooldown = "You can't use the bin yet, check back later."
 }
