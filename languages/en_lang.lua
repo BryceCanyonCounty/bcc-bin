4635c8ff45af1dddecd7592c577a17698280913b
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-12 23:20:21 -0700

    Update en_lang.lua

--- languages/en_lang.lua
+++ languages/en_lang.lua
@@ -1,15 +1,16 @@
 ---------------------------------- ENGLISH -------------------------------------------
 Locales["en_lang"] = {
     bin = "Bin",
-    search = 'Search',
-    progressbar = 'Searching Bin',
-    moneyfound = 'Found Money',
-    itemfound = 'Found an Item',
-    weaponfound = 'Found a Weapon',
-    webhookName = 'Firstname&Lastname: ',
-    webhookIdentifier = '\nIdentifier:',
-    webhookRewardM = '\nReward Money: ',
-    webhookRewardI = '\nReward Item: ',
-    webhookRewardW = '\nReward Weapon: ',
-    
+    search = "Search",
+    progressbar = "Searching Bin",
+    moneyfound = "Found Money: $",
+    itemfound = "Found an Item: ",
+    weaponfound = "Found a Weapon: ",
+    webhookName = "Firstname&Lastname: ",
+    webhookIdentifier = "\nIdentifier:",
+    webhookRewardM = "\nReward Money: ",
+    webhookRewardI = "\nReward Item: ",
+    webhookRewardW = "\nReward Weapon: ",
+    noSpace = "You do not have enough space in your inventory.",
+    useCooldown = "You can't use the bin yet, check back later."
 }
