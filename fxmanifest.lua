7b5c5d50b57ff63a919e49462dbb85003b7162b0
Andrew <10902965+AndrewR3K@users.noreply.github.com>
2024-11-09 11:46:38 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 game 'rdr3'
 lua54 'yes'
-author 'huzurweriN'
+author 'BCC Scripts @ huzurweriN'
 
 shared_scripts {
    'configs/*.lua',
@@ -25,4 +25,4 @@ dependencies {
    'feather-progressbar'
 }
 
-version '0.1.0'
+version '1.0.0'
